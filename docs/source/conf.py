79c6da300e303f31562a8f5d707e465f86d53d14
Luís Antunes <lr.antunes@gmail.com>
2016-12-22 13:16:39 +0000

    Project Documentation
    Fix documentation config file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,8 +47,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'MVC-RazorFeatures'
-copyright = '2016, Luís Antunes'
-author = 'Luís Antunes'
+copyright = '2016, Luis Antunes'
+author = 'Luis Antunes'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
